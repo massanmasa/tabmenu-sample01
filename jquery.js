9a96f140930cbfdf614a083d1a33567cd8a5e567
masamasa_uu <akatsukiryu848@gmail.com>
2018-07-24 22:17:22 +0900

    jquery.js - タブメニュークリックイベント追加

--- jquery.js
+++ jquery.js
@@ -1,8 +1,10 @@
 jQuery(function($) {
-    var index = $('.menu-item').index(this);
+    $('.menu-item').on('click', function() {
+        var index = $('.menu-item').index(this);
 
-    $('.menu-item').removeClass('active');
-    $(this).addClass('active');
+        $('.menu-item').removeClass('active');
+        $(this).addClass('active');
 
-    $('.tabs-contents').removeClass('active').eq(index).addClass('active');
+        $('.tabs-contents div').removeClass('active').eq(index).addClass('active');
+    });
 });
