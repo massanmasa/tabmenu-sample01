08b95259ca7c12e3c32f3a09d3a50840b455ed2d
masamasa_uu <akatsukiryu848@gmail.com>
2018-07-24 22:04:03 +0900

    jquery.js - タブメニュー切り替え処理追加

--- jquery.js
+++ jquery.js
@@ -1,2 +1,8 @@
 jQuery(function($) {
+    var index = $('.menu-item').index(this);
+
+    $('.menu-item').removeClass('active');
+    $(this).addClass('active');
+
+    $('.tabs-contents').removeClass('active').eq(index).addClass('active');
 });
